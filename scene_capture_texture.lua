46654b9a3319a58afc71e2b6e841bfa99c7cb147
Angaros <rga.quentin@gmail.com>
2024-11-18 16:57:35 +0100

    Added scene_capture_texture.py Renammed lua file

--- scene_capture_texture.lua
+++ scene_capture_texture.lua
@@ -5,7 +5,7 @@ hg.InputInit()
 hg.WindowSystemInit()
 
 res_x, res_y, tex_size = 1024, 1024, 1024
-win = hg.RenderInit('Frame Buffer Screenshot', res_x, res_y, hg.RF_VSync | hg.RF_MSAA4X)
+win = hg.RenderInit('Scene Capture Texture', res_x, res_y, hg.RF_VSync | hg.RF_MSAA4X)
 
 -- Link precompiled assets folder to the project
 hg.AddAssetsFolder("resources_compiled")
